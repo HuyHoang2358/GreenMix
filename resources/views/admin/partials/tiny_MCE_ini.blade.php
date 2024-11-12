e05404dda930d88795d3ec9afc2ab1440a869ed0
huyhoang2358 <huyhoang.tran6669@gmail.com>
2024-11-13 06:59:51 +0700

    fix tinymce vs unisharp

--- resources/views/admin/partials/tiny_MCE_ini.blade.php
+++ resources/views/admin/partials/tiny_MCE_ini.blade.php
@@ -20,7 +20,7 @@
             var y = window.innerHeight || document.documentElement.clientHeight || document
                 .getElementsByTagName('body')[0].clientHeight;
 
-            var cmsURL = editor_config.path_absolute + 'laravel-filemanager?editor=' + meta.fieldname;
+            var cmsURL = editor_config.path_absolute + 'admin/laravel-filemanager?editor=' + meta.fieldname;
             if (meta.filetype == 'image') {
                 cmsURL = cmsURL + "&type=Images";
             } else {
