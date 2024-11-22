c4e3ceefc5a7ad34f1e63599a214d2254e7456fc
Phong <tranxuanhoat2003@gmail.com>
2024-11-23 00:32:59 +0700

    Code footer

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Address;
 use App\Models\Partner;
 use Illuminate\Http\Request;
 
@@ -10,8 +11,10 @@ class HomeController extends Controller
     public function index()
     {
         $partners = Partner::all();
+        $addresses = Address::all();
         return view('homepage', [
             'partners' => $partners,
+            'addresses' => $addresses,
         ]);
     }
 }
